8f2c96b2bf08b6c6ea716a461e528f8913e064ab
michu990902 <41807744+michu990902@users.noreply.github.com>
2021-03-03 01:02:57 +0100

    Week/Month grid generator added

---
+++ src/components/Calendar/Calendar.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Calendar = () => {
+    return (
+        <div>
+            test
+        </div>
+    );
+};
+
+export default Calendar;
---
+++ src/lib/calendar.js
@@ -0,0 +1,50 @@
+export const getWeekGrid = (year, month, day) => {
+    let dayInWeek = new Date(year, month-1, day).getDay();
+    let i = 0;
+    let tmp = [];
+    while (i < 7){
+        tmp.push(new Date(year, month-1, day-dayInWeek+i).getDate());
+        i++;
+    }
+    return tmp;
+};
+
+export const getMonthGrid = (year, month) => {
+    const date = new Date(year, month, 0);
+    const monthLength = date.getDate();
+    date.setDate(1);
+    let firstDay = date.getDay(); //Sunday - Saturday : 0 - 6
+    let grid = [];
+    let tmpWeek = 0;
+    let total = 0;
+
+    const insertDayToWeek = day => {
+        total++;
+        const len = grid.length;
+        const lastItemID = len > 0 ? len - 1 : 0;
+        const lastItem = grid[lastItemID] || [];
+        if(tmpWeek > 0){
+            grid[lastItemID] = [...lastItem, day];
+            tmpWeek--;
+            return;
+        }
+        grid.push([day]);
+        tmpWeek = 6;
+    }
+
+    if(firstDay !== 0){
+        const tmpDate = new Date(year, month-1, 0);
+        const lastMonthLength = tmpDate.getDate();
+        while(firstDay){
+            const day = lastMonthLength - --firstDay;
+            insertDayToWeek(day);
+        }
+    }
+
+    let i = 1;
+    while(i < monthLength+1) insertDayToWeek(i++);
+    i = 1;
+    while(total%7 > 0) insertDayToWeek(i++);
+
+    return grid;
+};
